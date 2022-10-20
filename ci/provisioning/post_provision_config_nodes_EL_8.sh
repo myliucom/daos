fb9dc915b7ee0ce4841eb6a3a4249b511aef0c05
Brian J. Murrell <brian.murrell@intel.com>
2022-10-20 10:35:26 -0400

    SRE-549 test: Install MOFED ...
    during node provisioning, so that the network stack can be chosen per
    test run.
    
    Signed-off-by: Brian J. Murrell <brian.murrell@intel.com>
    Skip-PR-comments: true
    Quick-functional: true
    RPM-test-version: 2.3.101
    Provisioning-pool: bmurrell-automate

--- ci/provisioning/post_provision_config_nodes_EL_8.sh
+++ ci/provisioning/post_provision_config_nodes_EL_8.sh
@@ -18,88 +18,170 @@ distro_custom() {
     # for Launchable's pip install
     dnf -y install python3-setuptools.noarch
 
-    # New Rocky images don't have debuginfo baked into them
-    if ! dnf --enablerepo=\*-debuginfo repolist 2>/dev/null | grep -e -debuginfo; then
-        if [ "$(lsb_release -s -i)" = "Rocky" ]; then
-            # Need to remove the upstream [debuginfo] repos
-            # But need to have the files present so that re-installation is blocked
-            for file in /etc/yum.repos.d/{Rocky-Debuginfo,epel{,{,-testing}-modular}}.repo; do
-                true > $file
-            done
+    # Remove OPA and install MOFED
+    MLNX_VER_NUM=5.6-2.0.9.0
+    if [ -z "$MLNX_VER_NUM" ]; then
+        echo "MLNX_VER_NUM is not set"
+        env
+        exit 1
+    fi
 
-            # add local debuginfo repos
-            if [ -f /etc/yum.repos.d/daos_ci-rocky8-artifactory.repo ]; then
-                echo >> /etc/yum.repos.d/daos_ci-rocky8-artifactory.repo
-            fi
-
-            cat <<EOF >> /etc/yum.repos.d/daos_ci-rocky8-artifactory.repo
-[daos_ci-rocky8-base-artifactory-debuginfo]
-name=daos_ci-rocky8-base-artifactory-debuginfo
-baseurl=${ARTIFACTORY_URL}artifactory/rocky-\$releasever-proxy/BaseOS/\$arch/debug/tree/
-enabled=0
-gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rockyofficial
-
-[daos_ci-rocky8-appstream-artifactory-debuginfo]
-name=daos_ci-rocky8-appstream-artifactory-debuginfo
-baseurl=${ARTIFACTORY_URL}artifactory/rocky-\$releasever-proxy/AppStream/\$arch/debug/tree/
-enabled=0
-gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rockyofficial
-
-[daos_ci-rocky8-powertools-artifactory-debuginfo]
-name=daos_ci-rocky8-powertools-artifactory-debuginfo
-baseurl=${ARTIFACTORY_URL}artifactory/rocky-\$releasever-proxy/PowerTools/\$arch/debug/tree/
-enabled=0
-gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rockyofficial
-
-[daos_ci-rocky8-extras-artifactory-debuginfo]
-name=daos_ci-rocky8-extras-artifactory-debuginfo
-baseurl=${ARTIFACTORY_URL}artifactory/rocky-\$releasever-proxy/extras/\$arch/debug/tree/
-enabled=0
-gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rockyofficial
-EOF
+    # Remove omnipath software
+    # shellcheck disable=SC2046
+    dnf -y remove $(rpm -q opa-address-resolution \
+                           opa-basic-tools \
+                           opa-fastfabric \
+                           opa-libopamgt | grep -v 'is not installed')
+    
+    # shellcheck disable=SC2046
+    dnf -y remove $(rpm -q compat-openmpi16 \
+                           compat-openmpi16-devel \
+                           openmpi \
+                           openmpi-devel \
+                           ompi \
+                           ompi-debuginfo \
+                           ompi-devel | grep -v 'is not installed')
+    
+    if [ -e /usr/local/sbin/set_local_repos.sh ]; then
+        /usr/local/sbin/set_local_repos.sh artifactory
+    fi
+    
+    kernel_ver="$(uname -r)"
+    if ! dnf -y install bc \
+                        chkconfig \
+                        elfutils-libelf-devel \
+                        gcc gcc-gfortran gdb-headless \
+                        httpd \
+                        "kernel-devel-$kernel_ver" \
+                        "kernel-modules-extra-$kernel_ver" \
+                        kernel-rpm-macros \
+                        libnsl libxslt lsof \
+                        mariadb-server mod_ssl \
+                        net-snmp net-snmp-libs net-snmp-utils \
+                        pciutils perl php \
+                        python36-devel python3-pyOpenSSL \
+                        python3-virtualenv \
+                        rpm-build \
+                        tcl tcsh tk unixODBC; then
+        dnf repolist || true
+        dnf --showduplicates search kernel-{devel,modules_extra} || true
+        dnf --disablerepo=\* --enablerepo=daos_ci-alma8-base-artifactory repoquery -a || true
+        dnf --disablerepo=\* --enablerepo=daos_ci-alma8-powertools-artifactory repoquery -a || true
+        for f in /etc/yum.repos.d/*.repo; do
+            echo "--------- $f ---------"
+            cat "$f"
+        done
+        /usr/libexec/platform-python -c 'import dnf, json; db = dnf.dnf.Base(); print(json.dumps(db.conf.substitutions, indent=2))' || true
+        ls -l /etc/dnf/vars/
+        grep . /etc/dnf/vars/*
+        dnf -y install bc \
+                       chkconfig \
+                       elfutils-libelf-devel \
+                       gcc gcc-gfortran gdb-headless \
+                       httpd \
+                       kernel-devel \
+                       kernel-modules-extra \
+                       kernel-rpm-macros \
+                       libnsl libxslt lsof \
+                       mariadb-server mod_ssl \
+                       net-snmp net-snmp-libs net-snmp-utils \
+                       pciutils perl php \
+                       python36-devel python3-pyOpenSSL \
+                       python3-virtualenv \
+                       rpm-build \
+                       tcl tcsh tk unixODBC
+    fi
+    
+    dnf -y list --showduplicates perftest ucx-knem ucx
+    
+    stream=false
+    cversion="$(lsb_release -sr)"
+    if [ "$cversion" == "8" ]; then
+        gversion="8.6"
+        stream=true
+    else
+        if [[ $cversion = *.*.* ]]; then
+            gversion="${cversion%.*}"
         else
-            if [ -f /etc/yum.repos.d/daos_ci-centos8.repo ]; then
-                echo >> /etc/yum.repos.d/daos_ci-centos8.repo
-            fi
-
-            cat <<EOF >> /etc/yum.repos.d/daos_ci-centos8.repo
-[daos_ci-centos8-artifactory-debuginfo]
-name=daos_ci-centos8-artifactory-debuginfo
-baseurl=${ARTIFACTORY_URL}artifactory/centos-debuginfo-proxy/\$releasever/\$basearch/
-enabled=0
-gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-centosofficial
-EOF
+            gversion="$cversion"
         fi
     fi
-
-    # Mellanox OFED hack
-    if ls -d /usr/mpi/gcc/openmpi-*; then
-        version="$(rpm -q --qf "%{version}" openmpi)"
-        mkdir -p /etc/modulefiles/mpi/
-        cat <<EOF > /etc/modulefiles/mpi/mlnx_openmpi-x86_64
-#%Module 1.0
-#
-#  OpenMPI module for use with 'environment-modules' package:
-#
-conflict		mpi
-prepend-path 		PATH 		/usr/mpi/gcc/openmpi-${version}/bin
-prepend-path 		LD_LIBRARY_PATH /usr/mpi/gcc/openmpi-${version}/lib64
-prepend-path 		PKG_CONFIG_PATH	/usr/mpi/gcc/openmpi-${version}/lib64/pkgconfig
-prepend-path		MANPATH		/usr/mpi/gcc/openmpi-${version}/share/man
-setenv 			MPI_BIN		/usr/mpi/gcc/openmpi-${version}/bin
-setenv			MPI_SYSCONFIG	/usr/mpi/gcc/openmpi-${version}/etc
-setenv			MPI_FORTRAN_MOD_DIR	/usr/mpi/gcc/openmpi-${version}/lib64
-setenv			MPI_INCLUDE	/usr/mpi/gcc/openmpi-${version}/include
-setenv	 		MPI_LIB		/usr/mpi/gcc/openmpi-${version}/lib64
-setenv			MPI_MAN			/usr/mpi/gcc/openmpi-${version}/share/man
-setenv			MPI_COMPILER	openmpi-x86_64
-setenv			MPI_SUFFIX	_openmpi
-setenv	 		MPI_HOME	/usr/mpi/gcc/openmpi-${version}
-EOF
+    
+    # We need this temporarily on 8.4+
+    if [ "$gversion" != "8.3" ]; then
+        sudo dnf install --assumeyes compat-hwloc1 hwloc-devel
     fi
+    
+    #if $stream || [ "$gversion" = "8.6" ]; then
+    #  MLNX_VER_NUM="5.6-2.0.9.0"
+    #fi
+    # Try $gversion and one minor version lower
+    for mlnx_gversion in $gversion $(echo "scale=1; $gversion-.1" | bc); do
+        mlnx_ver="MLNX_OFED_LINUX-$MLNX_VER_NUM-rhel$mlnx_gversion-x86_64"
+        if wget -nv https://artifactory.dc.hpdd.intel.com/artifactory/raw-internal/mlnx_ofed/"$mlnx_ver".tgz; then
+            break
+        fi
+    done
+    
+    if [ ! -f "$mlnx_ver".tgz ]; then
+        echo "Failed to fetch a Mellanox tarball" >&2
+        exit 1
+    fi
+    
+    tar -xf "$mlnx_ver.tgz"
+    rm -f "$mlnx_ver.tgz"
+    
+    # Add a repo to install RPMS
+    dnf config-manager --add-repo="$mlnx_ver/RPMS/UPSTREAM_LIBS/"
+    rpm --import "$mlnx_ver"/RPM-GPG-KEY-Mellanox
+    dnf repolist || true
+
+    dnf -y install mlnx-ofed-all
+
+    # now, upgrade firmware
+    dnf -y install mlnx-fw-updater
+
+    grep ibacm /tmp/MLNX_OFED_LINUX.*/general.log || true
+    
+    tail -n +1 /tmp/MLNX_OFED_LINUX.*/general.log || true
+    
+    # Make sure that tools are present. 
+    ls /usr/bin/ib_* /usr/bin/ibv_*
+    
+    rm -rf "$mlnx_ver" ~/rpmbuild
+    
+    dnf list --showduplicates perftest
+    if [ "$gversion" == "8.5" ]; then
+        dnf remove -y perftest || true
+    fi
+    if $stream; then
+        dnf list --showduplicates ucx-knem
+        dnf remove -y ucx-knem || true
+    fi
+    
+    # Need this module file
+    version="$(rpm -q --qf "%{version}" openmpi)"
+    mkdir -p /etc/modulefiles/mpi/
+    cat << EOF > /etc/modulefiles/mpi/mlnx_openmpi-x86_64
+    #%Module 1.0
+    #
+    #  OpenMPI module for use with 'environment-modules' package:
+    #
+    conflict		mpi
+    prepend-path 		PATH 		/usr/mpi/gcc/openmpi-$version/bin
+    prepend-path 		LD_LIBRARY_PATH /usr/mpi/gcc/openmpi-$version/lib64
+    prepend-path 		PKG_CONFIG_PATH	/usr/mpi/gcc/openmpi-$version/lib64/pkgconfig
+    prepend-path		MANPATH		/usr/mpi/gcc/openmpi-$version/share/man
+    setenv 			MPI_BIN		/usr/mpi/gcc/openmpi-$version/bin
+    setenv			MPI_SYSCONFIG	/usr/mpi/gcc/openmpi-$version/etc
+    setenv			MPI_FORTRAN_MOD_DIR	/usr/mpi/gcc/openmpi-$version/lib64
+    setenv			MPI_INCLUDE	/usr/mpi/gcc/openmpi-$version/include
+    setenv	 		MPI_LIB		/usr/mpi/gcc/openmpi-$version/lib64
+    setenv			MPI_MAN			/usr/mpi/gcc/openmpi-$version/share/man
+    setenv			MPI_COMPILER	openmpi-x86_64
+    setenv			MPI_SUFFIX	_openmpi
+    setenv	 		MPI_HOME	/usr/mpi/gcc/openmpi-$version
+EOF
+    
+    printf 'MOFED_VERSION=%s\n' "$MLNX_VER_NUM" >> /etc/do-release
 }
